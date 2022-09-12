78d083fdf54923bd73f5dfcc95c9fdbdbf838833
Vansh <vansh8755381346@gmail.com>
2022-09-12 11:29:04 +0530

    utube icon

--- src/Comp/Head.jsx
+++ src/Comp/Head.jsx
@@ -31,6 +31,9 @@ export default function Head() {
               <a href='https://www.facebook.com/jdelegantinteriors/' className='me-4 text-reset'>
                 <i className='fab fa-facebook-f'></i>
               </a>
+              <a href='https://www.youtube.com/channel/UC6a0rRO9B3AX-5cnGSHIgeQ' className='me-4 text-reset'>
+                <i className='fab fa-youtube'></i>
+              </a>
               <a href='https://www.google.com/search?client=opera-gx&q=jd%20interiors&sourceid=opera&ie=UTF-8&oe=UTF-8&hs=GYd&tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALiCzsZqduR3MdOKucZqSy4BcHVnzXChBg:1662553848119&rflfq=1&num=10&rldimm=17897028522918420754&lqi=CgxqZCBpbnRlcmlvcnNI1PLqwdm4gIAIWhYQABABGAAYASIMamQgaW50ZXJpb3JzkgERaW50ZXJpb3JfZGVzaWduZXI&ved=2ahUKEwiwxKXc14L6AhULSmwGHS-sAaoQvS56BAgXEAE&sa=X&rlst=f#rlfi=hd:;si:17897028522918420754,l,CgxqZCBpbnRlcmlvcnNI1PLqwdm4gIAIWhYQABABGAAYASIMamQgaW50ZXJpb3JzkgERaW50ZXJpb3JfZGVzaWduZXI;mv:[[32.6711021,89.3895221],[8.4432678,72.2562176]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2' className='me-4 text-reset'>
                 <i className='fab fa-google'></i>
               </a>
