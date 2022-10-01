16688a098510c323cac4c0d0d4717298bc0d2f4d
Vansh <vansh8755381346@gmail.com>
2022-10-01 10:16:09 +0530

    css

---
+++ src/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+	return (
+		<>
+			<div className="contact">
+				<h1>Contact Us</h1>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3443.641683668163!2d78.05160671512188!3d30.332708281778544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390929cbe6ca5d89%3A0xf85f049e7b962112!2sJD%20Elegant%20Interiors!5e0!3m2!1sen!2sin!4v1664563737849!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</>
+	)
+}
